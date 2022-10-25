da3e8aeb7124cbb412ffd75849d80a14870a1458
Ripassorker <ripassorker02@glail.com>
2022-10-25 17:16:18 +0600

    added data details

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/courese.json");
+
 app.get("/", (req, res) => {
   res.send("online tech servar in running !!");
 });
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get('/courses/:id',(req,res)=>{
+    const id =req.params.id
+    
+})
 
 app.listen(port, () => {
   console.log(`Travel guru running on port, ${port}`);
